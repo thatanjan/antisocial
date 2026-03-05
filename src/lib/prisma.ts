d0633d2452406d12af23b0cdf2e7a51adf3eb21e
Anjan Shomodder <thatanjan@gmail.com>
2026-03-05 13:07:18 +0600

    Upgrade  Prisma to v7(latest) (#6)
    * deps: update Prisma packages to version 6.19.2.
    
    * Upgrade prisma to v7

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,29 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+import { PrismaClient } from "../generated/client/client";
 
 declare global {
+  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
+  // eslint-disable-next-line no-var
+  var pool: Pool | undefined;
 }
 
-const db = globalThis.prisma || new PrismaClient();
+const connectionString = process.env.DATABASE_URL;
 
-if (process.env.NODE_ENV !== "production") {
-  globalThis.prisma = db;
+let db: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  const pool = new Pool({ connectionString });
+  const adapter = new PrismaPg(pool);
+  db = new PrismaClient({ adapter });
+} else {
+  if (!globalThis.prisma) {
+    globalThis.pool = new Pool({ connectionString });
+    const adapter = new PrismaPg(globalThis.pool as Pool);
+    globalThis.prisma = new PrismaClient({ adapter });
+  }
+  db = globalThis.prisma;
 }
 
 export default db;
